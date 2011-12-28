80f59ad75cb45b7faa000e9d758227ae5fc084ac
Brett Viren <bv@bnl.gov>
2011-12-28 13:50:26 -0500

    Pass any proxy variables from user to construction environment

--- python/metascons/util.py
+++ python/metascons/util.py
@@ -15,6 +15,17 @@ def guess_platform():
 
     return '-'.join(plat)
 
+def fix_env(env):
+    env['ENV']['PATH'] = env['ENV']['PATH'].split(':')
+    for ptype in ['http','ftp','https','all','no']:
+        pname = ptype + '_proxy'
+        for var in [pname, pname.upper()]:
+            val = os.environ.get(var)
+            if not val: continue
+            env['ENV'][var] = val
+            continue
+        continue
+    return env
 
 if '__main__' == __name__:
     print guess_platform()
